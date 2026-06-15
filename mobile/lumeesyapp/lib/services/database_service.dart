93775f4655955f74854cb6d0968cfdd87853c5d8
whosmitri <hellomitrii@gmail.com>
2026-06-15 10:50:32 -0300

    feat(mobile): crie métodos para cadastro de planta

--- mobile/lumeesyapp/lib/services/database_service.dart
+++ mobile/lumeesyapp/lib/services/database_service.dart
@@ -26,6 +26,7 @@ class DatabaseService {
   static const String _plantas = 'plantas';
   static const String _especies = 'especies_catalogo';
 
+  // USUÁRIOS
   Future<bool> usuarioPossuiPlanta(String uid) async {
     try {
       final userDoc =
@@ -49,5 +50,54 @@ class DatabaseService {
     } catch (_) {
       return false;
     }
+  }
+
+  // ESPÉCIES
+  Future<List<Map<String, dynamic>>> buscarEspecies() async {
+    final snapshot = await _db.collection(_especies).get();
+
+    return snapshot.docs.map((doc) {
+      return doc.data();
+    }).toList();
+  }
+
+  // PLANTAS
+  Future<String?> cadastrarPlanta({
+    required String uid,
+    required String nomeApelido,
+    required String idEspecie,
+  }) async {
+    try {
+      final plantaRef = _db.collection(_plantas).doc();
+
+      await plantaRef.set({
+        'id_planta': plantaRef.id,
+        'uid': uid,
+        'nome_apelido': nomeApelido,
+        'id_especie': idEspecie,
+        'ultima_leitura': {
+          'timestamp': null,
+          'umidade_solo_bruto': 0,
+          'umidade_solo_porcentagem': 0,
+          'luminosidade': 0,
+          'temperatura_ar': 0,
+          'umidade_ar': 0,
+        },
+      });
+
+      return plantaRef.id;
+    } catch (_) {
+      return null;
+    }
+  }
+
+  // USUÁRIOS
+  Future<void> atualizarIdPlantaUsuario({
+    required String uid,
+    required String idPlanta,
+  }) async {
+    await _db.collection(_usuarios).doc(uid).update({
+      'id_planta': idPlanta,
+    });
   }
 }
